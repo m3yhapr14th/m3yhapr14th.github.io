96bf1c7af62e5543375d8b202b6ee469b7aa36f2
m3yhapr14th <m3yhapr14th@smime.ninja>
2022-01-08 14:06:20 +0900

    blog

--- rf.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-cert_url = requests.get("https://www.krcert.or.kr/data/secNoticeList.do")
-soup = BeautifulSoup(cert_url.text, "html.parser")
-page_get = soup.find("div", {"class":"paging rwdP"})
-table_get = soup.find("table", {"class":"basicList default"})
